2efd15154488cdbdfc29522da0e74d98719666d9
Oguzcan <oguzcansirolu1@gmail.com>
2025-03-20 18:37:00 +0300

    snakecase

--- ilet.Server/Context/AppDbContext.cs
+++ ilet.Server/Context/AppDbContext.cs
@@ -10,5 +10,27 @@ namespace ilet.Server.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                // Tablo ismini snake_case yap
+                entity.SetTableName(ToSnakeCase(entity.GetTableName()!));
+
+                // Kolonları snake_case yap
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(ToSnakeCase(property.Name));
+                }
+            }
+        }
+        private static string ToSnakeCase(string input)
+        {
+            return string.Concat(
+                input.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())
+            ).ToLower();
+        }
     }
 }
