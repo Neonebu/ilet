620dfa7938e3a3902f4c241320a771393be32b18
Oguzcan <oguzcansirolu1@gmail.com>
2025-03-22 00:59:56 +0300

    cors

--- ilet.Server/Program.cs
+++ ilet.Server/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
+              .AllowAnyMethod();
     });
 });
 
