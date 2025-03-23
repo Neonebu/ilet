09c97ea371448e7e62da85db9ec58342913b00ac
Oguzcan <oguzcansirolu1@gmail.com>
2025-03-23 18:22:41 +0300

    fix

--- ilet.client/src/components/StatusDropdown.tsx
+++ ilet.client/src/components/StatusDropdown.tsx
@@ -1,7 +1,7 @@
 ﻿import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
-export default function StatusDropdown({status, setStatus }: { status: string, setStatus: (s: string) => void }) {
+export default function StatusDropdown({ status, setStatus }: { status: string, setStatus: (s: string) => void }) {
     const { t } = useTranslation();
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
@@ -13,10 +13,10 @@ export default function StatusDropdown({status, setStatus }: { status: string, s
             <span className="dropdown-arrow">▼</span>
             {dropdownOpen && (
                 <ul className="status-dropdown">
-                    <li onClick={() => setStatus("Online")}>Online</li>
-                    <li onClick={() => setStatus("Busy")}>Busy</li>
-                    <li onClick={() => setStatus("Away")}>Away</li>
-                    <li onClick={() => setStatus("Invisible")}>Invisible</li>
+                    <li onClick={() => setStatus("Online")}>{t('Online')}</li>
+                    <li onClick={() => setStatus("Busy")}>{t('Busy')}</li>
+                    <li onClick={() => setStatus("Away")}>{t('Away')}</li>
+                    <li onClick={() => setStatus("Invisible")}>{t('Invisible')}</li>
                 </ul>
             )}
         </div>
