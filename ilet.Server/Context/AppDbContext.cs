ce39807639f1dd8b59a478ea8b0e9f8783d9052a
Oguzcan <oguzcansirolu1@gmail.com>
2025-03-24 01:53:55 +0300

    dbcontext

--- ilet.Server/Context/AppDbContext.cs
+++ ilet.Server/Context/AppDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using ilet.Server.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace ilet.Server.Context;
 
@@ -21,8 +22,7 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<Users> User { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=dpg-cvc71llds78s73ag5g9g-a.oregon-postgres.render.com;Port=5432;Database=iletapi_db;Username=iletapi_db_user;Password=9FDT9Cv3POHCkXRmDWRAcSekKssHYdQG;Ssl Mode=Require;Trust Server Certificate=true;");
+        => optionsBuilder.UseNpgsql("Host=dpg-cvc71llds78s73ag5g9g-a.oregon-postgres.render.com;Port=5432;Database=iletapi_db;Username=iletapi_db_user;Password=9FDT9Cv3POHCkXRmDWRAcSekKssHYdQG;Ssl Mode=Require;Trust Server Certificate=true;").ConfigureWarnings(x => x.Ignore(RelationalEventId.PendingModelChangesWarning));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
