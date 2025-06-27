cf819f498ebc7a15d6e26faa8f09132634f7b897
Oguzcan <oguzcansirolu1@gmail.com>
2025-06-27 17:43:37 +0300

    copy static js

---
+++ ilet.client/copy-static.js
@@ -0,0 +1,9 @@
+﻿// copy-static.js
+import { copyFileSync } from 'fs';
+import { join } from 'path';
+
+const from = join(process.cwd(), 'static.json');
+const to = join(process.cwd(), 'dist', 'static.json');
+
+copyFileSync(from, to);
+console.log('✅ static.json copied to dist/');
